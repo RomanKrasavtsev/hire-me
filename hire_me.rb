2fbdb398a0f6d4fa909d49c07198d6eaecca7a25
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2016-10-26 00:01:40 +0300

    update hire_me

--- hire_me.rb
+++ hire_me.rb
@@ -1,4 +1,9 @@
 #!/usr/bin/env ruby
 
 today = Time.now
-puts "git commit -m '#{today}' --date='#{today}'"
+
+(0..366).to_a.each do |i|
+    shift = i * 24 * 60 * 60
+    day = today - shift
+    puts "git commit -m '#{day}' --date='#{day}'"
+end
